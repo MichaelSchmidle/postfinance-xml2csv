6d0673f57bc667c0f784b9e2f1ed797396f5eeaa
Michael Schmidle <michael@schmidle.net>
2018-05-08 20:02:57 +0200

    Added date to csv filename

--- postfinance-xml2csv.py
+++ postfinance-xml2csv.py
@@ -2,6 +2,7 @@ import os
 import glob
 import xml.etree.ElementTree as eTree
 import re
+from datetime import datetime
 import csv
 
 # Function to extract XML namespace from element
@@ -27,9 +28,10 @@ for xmlFile in xmlFiles:
     # Define metadata
     iban = root.find('Document:BkToCstmrStmt/Document:Stmt/Document:Acct/Document:Id/Document:IBAN', ns).text
     owner = root.find('Document:BkToCstmrStmt/Document:Stmt/Document:Acct/Document:Ownr/Document:Nm', ns).text
+    closingDate = root.findall('Document:BkToCstmrStmt/Document:Stmt/Document:Bal', ns)[1].find('Document:Dt/Document:Dt', ns).text
 
     # Generate corresponding CSV file with header row
-    with open(baseDir + 'csv/' + iban + os.path.basename(xmlFile) + '.csv', 'w') as csvFile:
+    with open(baseDir + 'csv/' + iban + '-' + closingDate + '-' + os.path.basename(xmlFile) + '.csv', 'w') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerow(['IBAN', 'Owner', 'Amount', 'Currency', 'Date', 'Info', 'Reference'])
         transactions = root.findall('Document:BkToCstmrStmt/Document:Stmt/Document:Ntry', ns)
